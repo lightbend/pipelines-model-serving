20df3bcd46f61b776b792ffd211653a1a4afd54d
blublinsky <boris.lublinsky@lightbend.com>
2019-05-15 17:19:36 -0500

    Added TF serving and recommender models

--- model-serving-flow/src/main/scala/pipelines/examples/modelserving/recommedermodel/tensorflow/RecommenderTensorflowServingModel.scala
+++ model-serving-flow/src/main/scala/pipelines/examples/modelserving/recommedermodel/tensorflow/RecommenderTensorflowServingModel.scala
@@ -1,5 +1,6 @@
 package pipelines.examples.modelserving.recommedermodel.tensorflow
 
+import com.google.gson.Gson
 import com.lightbend.modelserving.model.{Model, ModelFactory, ModelToServe}
 import pipelines.examples.data.{ProductPrediction, RecommenderRecord}
 import com.lightbend.modelserving.model.tensorflow.TensorFlowServingModel
@@ -20,6 +21,15 @@ class RecommenderTensorflowServingModel(inputStream: Array[Byte]) extends
       .zip (input.products) map (r => ProductPrediction(r._2, r._1))
   }
 
+  // Test method to ensure that transformation works correctly
+  def transformer(input: RecommenderRecord, servingResult : String): Unit = {
+
+    val hTTPRec = gson.toJson(getHTTPRequest(input))
+    println(s" input to json : $hTTPRec")
+    val res = gson.fromJson(servingResult, clazz)
+    val result = getResult(res, input)
+    println(s"execution result $result")
+  }
 }
 
 /**
@@ -48,5 +58,17 @@ object RecommenderTensorflowServingModel extends ModelFactory[RecommenderRecord,
     */
   override def restore(bytes: Array[Byte]): Model[RecommenderRecord, Seq[ProductPrediction]] =
     new RecommenderTensorflowServingModel(bytes)
+
+  // Testing transformation
+  def main(args: Array[String]): Unit = {
+
+    val gson = new Gson
+
+    val model = new RecommenderTensorflowServingModel("test url".getBytes())
+    val record = new RecommenderRecord(10L, Seq(1L,2L,3L,4L), "recommeder")
+    val httpRes = gson.toJson(new TFPredictionResult(new RecommendationOutputs(Seq(1,2,3).toArray,
+      Seq(Seq(.1).toArray, Seq(.2).toArray, Seq(.3).toArray, Seq(.4).toArray).toArray)))
+    model.transformer(record, httpRes)
+  }
 }
 
