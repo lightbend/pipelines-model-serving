01720e0bf35724e078d598a12bafafafb9b1f10b
Dean Wampler <dean@concurrentthought.com>
2019-07-25 20:42:38 -0500

    tweaks

--- wine-quality-ml/src/main/scala/pipelines/examples/model-serving/wine-quality/WineDataIngressMain.scala
+++ wine-quality-ml/src/main/scala/pipelines/examples/model-serving/wine-quality/WineDataIngressMain.scala
@@ -9,16 +9,47 @@ import akka.stream.scaladsl.Sink
  */
 object WineDataIngressMain {
 
-  /** For testing purposes. */
+  /**
+   * For testing purposes.
+   * At this time, Pipelines intercepts calls to sbt run and sbt runMain, so use
+   * the console instead:
+   * ```
+   * import pipelines.examples.modelserving.winequality._
+   * WineDataIngressMain.main(Array("-n", "5"))
+   * ```
+   */
   def main(args: Array[String]): Unit = {
-    var count = if (args.length > 0) args(0).toInt else 1000
+    val defaultN = 100
+    val defaultF = WineDataIngressUtil.dataFrequencyMilliseconds
+    def help() = println(s"""
+      |usage: WineDataIngressMain [-h|--help] [-n|--count N] [-f|--frequency F]
+      |where:
+      |  -h | --help         print this message and exit
+      |  -n | --count N      print N records and stop (default: $defaultN)
+      |  -f | --frequency F  seconds between output model descriptions (default: $defaultF)
+      |""".stripMargin)
+
+    def parseArgs(args2: Seq[String], nf: (Int,Int)): (Int,Int) = args2 match {
+      case ("-h" | "--help") +: _ ⇒
+        help()
+        sys.exit(0)
+      case Nil                                 ⇒ nf
+      case ("-n" | "--count") +: n +: tail ⇒ parseArgs(tail, (n.toInt, nf._2))
+      case ("-f" | "--frequency") +: n +: tail ⇒ parseArgs(tail, (nf._1, n.toInt.seconds))
+      case x +: _ ⇒
+        println(s"ERROR: Unrecognized argument $x. All args: ${args.mkString(" ")}")
+        help()
+        sys.exit(1)
+    }
+
+    val (count, frequency) = parseArgs(args, (defaultN, defaultF))
+
     println(s"printing $count records")
-    println(s"frequency (seconds): ${WineDataIngressUtil.dataFrequencyMilliseconds}")
-    implicit val system = ActorSystem("RecommenderDataIngressMain")
+    println(s"frequency (seconds): $frequency")
+    implicit val system = ActorSystem("WineDataIngressMain")
     implicit val mat = ActorMaterializer()
     val source = WineDataIngressUtil.makeSource(
-      WineDataIngressUtil.rootConfigKey,
-      WineDataIngressUtil.dataFrequencyMilliseconds)
+      WineDataIngressUtil.rootConfigKey, frequency)
     source.runWith {
       Sink.foreach { line ⇒
         println(line.toString)
