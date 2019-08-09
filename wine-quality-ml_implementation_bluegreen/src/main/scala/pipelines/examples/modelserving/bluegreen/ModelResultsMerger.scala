8b3f9401850a79c38a44c5fea94061f68e4719a2
blublinsky <boris.lublinsky@lightbend.com>
2019-08-09 13:05:45 -0500

    build passes - WIP

---
+++ wine-quality-ml_implementation_bluegreen/src/main/scala/pipelines/examples/modelserving/bluegreen/ModelResultsMerger.scala
@@ -0,0 +1,17 @@
+package pipelines.examples.modelserving.bluegreen
+
+import pipelines.streamlets._
+import pipelines.akkastream._
+import pipelines.akkastream.util.scaladsl.MergeLogic
+import pipelines.examples.modelserving.winequality.data.WineResult
+import pipelines.streamlets.avro._
+
+class ModelResultsMerger extends AkkaStreamlet {
+
+  val in0 = AvroInlet[WineResult]("in-0")
+  val in1 = AvroInlet[WineResult]("in-1")
+  val out = AvroOutlet[WineResult]("out", _.inputRecord.lot_id)
+
+  final override val shape = StreamletShape.withInlets(in0, in1).withOutlets(out)
+  override final def createLogic = new MergeLogic(Vector(in0, in1), out)
+}
--- wine-quality-ml_implementation_bluegreen/src/main/scala/pipelines/examples/modelserving/bluegreen/ModelSplitIngress.scala
+++ wine-quality-ml_implementation_bluegreen/src/main/scala/pipelines/examples/modelserving/bluegreen/ModelSplitIngress.scala
@@ -8,7 +8,9 @@ import pipelines.streamlets.avro.AvroOutlet
 import pipelines.streamlets.StreamletShape
 
 import scala.concurrent.duration._
-import com.lightbend.modelserving.splitter.StreamSplitter
+import com.lightbend.modelserving.splitter.{ OutputPercentage, StreamSplitter }
+
+import scala.collection.mutable.ListBuffer
 
 /**
  * Ingress of model splitting updates. In this case, every two minutes we load and
@@ -28,7 +30,28 @@ final case object ModelSplitIngress extends AkkaStreamlet {
 
 object ModelSplitIngressUtil {
 
+  val percentage1 = Array(10, 90)
+  val percentage2 = Array(40, 60)
+  val percentage3 = Array(60, 40)
+  val percentage4 = Array(90, 10)
+  val percentageDefs = List(percentage1, percentage2, percentage3, percentage4)
+  var iterator = percentageDefs.iterator
+
   /** Helper method extracted from RecommenderModelIngress for easier unit testing. */
   def makeSource(
-      frequency: FiniteDuration = 120.seconds): Source[StreamSplitter, NotUsed] = ???
+      frequency: FiniteDuration = 120.seconds): Source[StreamSplitter, NotUsed] =
+    Source.repeat(generateSplit())
+      .throttle(1, frequency)
+
+  def generateSplit(): StreamSplitter = {
+    if (!iterator.hasNext) iterator = percentageDefs.iterator
+    val percents = iterator.next()
+    val inputs = new ListBuffer[OutputPercentage]()
+    0 to 1 foreach (i ⇒ {
+      val output = i
+      val percentage = percents(i)
+      inputs += new OutputPercentage(output, percentage)
+    })
+    new StreamSplitter(inputs)
+  }
 }
